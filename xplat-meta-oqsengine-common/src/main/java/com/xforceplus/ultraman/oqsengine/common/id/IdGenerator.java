13c1daf56cf645256ff7ae829af58545f7385b70
董斌 <dongbing@xforceplus.com>
2020-02-17 11:45:03 +0800

    1. 增加了 id 生成接口.和 Long类型id 生成的空定义.

---
+++ xplat-meta-oqsengine-common/src/main/java/com/xforceplus/ultraman/oqsengine/common/id/IdGenerator.java
@@ -0,0 +1,11 @@
+package com.xforceplus.ultraman.oqsengine.common.id;
+
+/**
+ * @author dongbin
+ * @version 0.1 2020/2/16 22:39
+ * @since 1.8
+ */
+public interface IdGenerator<V> {
+
+    V next();
+}
---
+++ xplat-meta-oqsengine-common/src/main/java/com/xforceplus/ultraman/oqsengine/common/id/LongIdGenerator.java
@@ -0,0 +1,12 @@
+package com.xforceplus.ultraman.oqsengine.common.id;
+
+import com.sun.xml.internal.bind.v2.model.core.ID;
+
+/**
+ * @author dongbin
+ * @version 0.1 2020/2/16 22:40
+ * @since 1.8
+ */
+public interface LongIdGenerator extends IdGenerator<Long> {
+
+}
