1fc55715c4f09503ce9e8618ea09450036c8ba2b
luye <luye66@163.com>
2020-02-17 15:37:03 +0800

    fix test unit

--- xplat-meta-oqsengine-common/src/test/java/com/xforceplus/ultraman/oasengine/common/id/test/TestFlake.java
+++ xplat-meta-oqsengine-common/src/test/java/com/xforceplus/ultraman/oasengine/common/id/test/TestFlake.java
@@ -3,6 +3,7 @@ package com.xforceplus.ultraman.oasengine.common.id.test;
 import com.xforceplus.ultraman.oqsengine.common.id.SnowflakeIdGenerator;
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.stream.IntStream;
 
 public class TestFlake {
@@ -11,12 +12,15 @@ public class TestFlake {
     public void testGenID() throws InterruptedException {
         SnowflakeIdGenerator generator = new SnowflakeIdGenerator(1);
 
+        CountDownLatch latch = new CountDownLatch(5000);
+
         IntStream.range(0 , 5000)
                 .mapToObj(i -> new Thread(() -> {
                     System.out.println(generator.next());
+                    latch.countDown();
                 }))
                 .forEach(Thread::start);
 
-        Thread.sleep(10000);
+       latch.await();
     }
 }
