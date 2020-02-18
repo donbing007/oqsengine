37bb565512fb55b66fb080e9aeb4ddc55cae574d
董斌 <dongbing@xforceplus.com>
2020-02-18 21:52:48 +0800

    1. 修改了定义.

--- xplat-meta-oqsengine-core/src/main/java/com/xforceplus/ultraman/oqsengine/core/metadata/IValue.java
+++ xplat-meta-oqsengine-core/src/main/java/com/xforceplus/ultraman/oqsengine/core/metadata/IValue.java
@@ -1,27 +1,22 @@
 package com.xforceplus.ultraman.oqsengine.core.metadata;
 
+/**
+ * 表示一个 entity 的属性值.
+ * @param <T> 实际类型.
+ */
 public interface IValue<T> {
 
     /**
      * 获得属性名
-     * @return
+     * @return 属性名称.
      */
     public String getName();
 
     /**
      * 根据属性名拿到值
-     * @param name
-     * @return
+     * @return 值.
      */
-    public T getValue(String name);
-
-    /**
-     * 设置属性名和值
-     * @param name
-     * @param value
-     * @return
-     */
-    public T setValue(String name ,T value);
+    public T getValue();
 
     /**
      * 将值转成String输出，所有类型都可以转成String
@@ -33,6 +28,6 @@ public interface IValue<T> {
      * 将值转成Long输出，String类型不允许转成Long，其他类型可以转成Long
      * @return
      */
-    public Long valueToLong();
+    public long valueToLong();
 
 }
