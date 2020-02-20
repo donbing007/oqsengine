ad4132fe92d5a338712e1b4df5254a50770dc2f5
unknown <wangzheng@xforceplus.com>
2020-02-20 18:19:18 +0800

    调整转换Page的属性信息

--- xplat-meta-oqsengine-sdk/src/main/java/com/xforceplus/ultraman/oqsengine/sdk/vo/Page.java
+++ xplat-meta-oqsengine-sdk/src/main/java/com/xforceplus/ultraman/oqsengine/sdk/vo/Page.java
@@ -7,9 +7,10 @@ import java.io.ObjectInput;
 import java.io.ObjectOutput;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 /**
- * 分页对象.
+ * 分页转换对象
  * @version 0.1 2020/2/13 15:30
  * @author wangzheng
  * @since 1.8
@@ -23,7 +24,7 @@ public class Page<T> implements Externalizable, Cloneable {
      */
     private List<T> rows = Collections.emptyList();
     /**
-     * 統計數據
+     * 统计数据
      */
     private Summary summary = new Summary();
     /**
@@ -36,14 +37,71 @@ public class Page<T> implements Externalizable, Cloneable {
      */
     private long current = 1;
 
+    public Page(List<T> rows, Summary summary, long size, long current) {
+        this.rows = rows;
+        this.summary = summary;
+        this.size = size;
+        this.current = current;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public List<T> getRows() {
+        return rows;
+    }
+
+    public void setRows(List<T> rows) {
+        this.rows = rows;
+    }
+
+    public Summary getSummary() {
+        return summary;
+    }
+
+    public void setSummary(Summary summary) {
+        this.summary = summary;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public long getCurrent() {
+        return current;
+    }
+
+    public void setCurrent(long current) {
+        this.current = current;
+    }
 
     @Override
     public void writeExternal(ObjectOutput out) throws IOException {
 
     }
-
     @Override
     public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Page)) return false;
+        Page<?> page = (Page<?>) o;
+        return getSize() == page.getSize() &&
+                getCurrent() == page.getCurrent() &&
+                Objects.equals(getRows(), page.getRows()) &&
+                Objects.equals(getSummary(), page.getSummary());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getRows(), getSummary(), getSize(), getCurrent());
     }
 }
