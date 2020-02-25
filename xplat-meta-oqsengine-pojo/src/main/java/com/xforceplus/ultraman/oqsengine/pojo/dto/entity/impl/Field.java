7ec3eb1efaf00a08c1b0b63edb12f6336940bccd
unknown <wangzheng@xforceplus.com>
2020-02-25 22:12:13 +0800

    添加字段属性信息，添加关系中的JoinBo的id信息

--- xplat-meta-oqsengine-pojo/src/main/java/com/xforceplus/ultraman/oqsengine/pojo/dto/entity/impl/Field.java
+++ xplat-meta-oqsengine-pojo/src/main/java/com/xforceplus/ultraman/oqsengine/pojo/dto/entity/impl/Field.java
@@ -3,6 +3,7 @@ package com.xforceplus.ultraman.oqsengine.pojo.dto.entity.impl;
 
 import com.xforceplus.ultraman.oqsengine.pojo.dto.entity.FieldType;
 import com.xforceplus.ultraman.oqsengine.pojo.dto.entity.IEntityField;
+import com.xforceplus.ultraman.oqsengine.pojo.dto.values.IValue;
 
 import java.io.Serializable;
 import java.util.Objects;
@@ -31,56 +32,123 @@ public class Field implements IEntityField, Serializable {
      */
     private FieldType fieldType;
 
+    /**
+     * 字段是否可搜索
+     */
+    private boolean searchType;
+
+    /**
+     * 字段数据最大长度
+     */
+    private int maxSize;
+
+    /**
+     * 字段数据最小长度
+     */
+    private int mixSize;
+
     public Field() {
     }
 
-    public Field(long id, String name, FieldType fieldType) {
+    public Field(long id, String name, FieldType fieldType, boolean searchType, int maxSize, int mixSize) {
         this.id = id;
         this.name = name;
         this.fieldType = fieldType;
+        this.searchType = searchType;
+        this.maxSize = maxSize;
+        this.mixSize = mixSize;
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public FieldType getFieldType() {
+        return fieldType;
+    }
+
+    public void setFieldType(FieldType fieldType) {
+        this.fieldType = fieldType;
+    }
+
+    public boolean isSearchType() {
+        return searchType;
+    }
+
+    public void setSearchType(boolean searchType) {
+        this.searchType = searchType;
+    }
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public void setMaxSize(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public int getMixSize() {
+        return mixSize;
+    }
+
+    public void setMixSize(int mixSize) {
+        this.mixSize = mixSize;
+    }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Field)) {
-            return false;
-        }
+        if (this == o) return true;
+        if (!(o instanceof Field)) return false;
         Field field = (Field) o;
-        return id == field.id &&
-            Objects.equals(name, field.name) &&
-            fieldType == field.fieldType;
+        return getId() == field.getId() &&
+                isSearchType() == field.isSearchType() &&
+                getMaxSize() == field.getMaxSize() &&
+                getMixSize() == field.getMixSize() &&
+                Objects.equals(getName(), field.getName()) &&
+                getFieldType() == field.getFieldType();
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, fieldType);
+        return Objects.hash(getId(), getName(), getFieldType(), isSearchType(), getMaxSize(), getMixSize());
     }
 
     @Override
     public String toString() {
         return "Field{" +
-            "id=" + id +
-            ", name='" + name + '\'' +
-            ", fieldType=" + fieldType +
-            '}';
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", fieldType=" + fieldType +
+                ", searchType=" + searchType +
+                ", maxSize=" + maxSize +
+                ", mixSize=" + mixSize +
+                '}';
     }
 
     @Override
     public long id() {
-        return id;
+        return this.id;
     }
 
     @Override
     public String name() {
-        return name;
+        return this.name;
     }
 
     @Override
     public FieldType type() {
-        return fieldType;
+        return this.fieldType;
     }
 }
