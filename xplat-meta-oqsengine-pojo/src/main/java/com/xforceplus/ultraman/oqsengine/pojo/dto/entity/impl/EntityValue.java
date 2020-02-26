7bc33267fa1e3f2c2fd736cca24242692c9cc053
董斌 <dongbing@xforceplus.com>
2020-02-26 21:37:23 +0800

    1. 修正同步删除的问题.

--- xplat-meta-oqsengine-pojo/src/main/java/com/xforceplus/ultraman/oqsengine/pojo/dto/entity/impl/EntityValue.java
+++ xplat-meta-oqsengine-pojo/src/main/java/com/xforceplus/ultraman/oqsengine/pojo/dto/entity/impl/EntityValue.java
@@ -70,7 +70,7 @@ public class EntityValue implements IEntityValue,Cloneable, Serializable {
 
     @Override
     public void filter(Predicate<? super IValue> predicate) {
-        Collection<IValue> snapshot = values.values();
+        Collection<IValue> snapshot = new ArrayList(values.values());
         for (IValue v : snapshot) {
             if (!predicate.test(v)) {
                 values.remove(v.getField());
