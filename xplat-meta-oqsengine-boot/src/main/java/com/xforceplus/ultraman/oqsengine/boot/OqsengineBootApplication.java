6d04cd6c78f20591d2e11542e72c29d1d3e885a9
董斌 <dongbing@xforceplus.com>
2020-02-28 19:26:32 +0800

    1. 修正事务管理器实现,现在会打印事务生命周期的日志. 2. 修正了 sphinxQL 事务删除的失败的问题.

--- xplat-meta-oqsengine-boot/src/main/java/com/xforceplus/ultraman/oqsengine/boot/OqsengineBootApplication.java
+++ xplat-meta-oqsengine-boot/src/main/java/com/xforceplus/ultraman/oqsengine/boot/OqsengineBootApplication.java
@@ -1,21 +1,7 @@
 package com.xforceplus.ultraman.oqsengine.boot;
 
-import com.xforceplus.ultraman.oqsengine.core.service.EntityManagementService;
-import com.xforceplus.ultraman.oqsengine.pojo.dto.entity.FieldConfig;
-import com.xforceplus.ultraman.oqsengine.pojo.dto.entity.FieldType;
-import com.xforceplus.ultraman.oqsengine.pojo.dto.entity.IEntityField;
-import com.xforceplus.ultraman.oqsengine.pojo.dto.entity.impl.Entity;
-import com.xforceplus.ultraman.oqsengine.pojo.dto.entity.impl.EntityClass;
-import com.xforceplus.ultraman.oqsengine.pojo.dto.entity.impl.EntityValue;
-import com.xforceplus.ultraman.oqsengine.pojo.dto.entity.impl.Field;
-import com.xforceplus.ultraman.oqsengine.pojo.dto.values.LongValue;
-import com.xforceplus.ultraman.oqsengine.pojo.dto.values.StringValue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * 接收参数 -Dds={数据源配置路径},或者当前类路径下有"oqsengine-ds.conf" 文件.
