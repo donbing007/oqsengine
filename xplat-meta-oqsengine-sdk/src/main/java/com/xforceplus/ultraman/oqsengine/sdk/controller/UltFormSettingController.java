a3fbf561bf916462021cbfe4d0e57963b76ac096
unknown <wangzheng@xforceplus.com>
2020-02-28 16:39:41 +0800

    提交部署 - 修改了页面配置的二次查询

--- xplat-meta-oqsengine-sdk/src/main/java/com/xforceplus/ultraman/oqsengine/sdk/controller/UltFormSettingController.java
+++ xplat-meta-oqsengine-sdk/src/main/java/com/xforceplus/ultraman/oqsengine/sdk/controller/UltFormSettingController.java
@@ -45,6 +45,118 @@ public class UltFormSettingController {
      */
     @PostMapping("/form-settings/{id}/deployments" )
     public Response deploymentsForm(@PathVariable String id) {
+        Response<List<UltForm>> result = initSeetings(id);
+        return result;
+//        String accessUri = "http://pfcp.phoenix-t.xforceplus.com";
+//        String url = String.format("%s/forms/%s/deployments"
+//                , accessUri
+//                , id);
+//        Authorization auth = new Authorization();
+//        auth.setAppId(Long.parseLong(config.getAppId()));
+////        auth.setTenantId(Long.parseLong(config.getTenant()));
+//        auth.setEnv(config.getEnv());
+//        Response<List<UltForm>> result = new Response<List<UltForm>>();
+//        try {
+////            result = restTemplate.getForObject(url,Response.class);
+//            result = restTemplate.postForObject(url, auth,Response.class);
+//            if (result.getResult()!=null){
+//                List<UltForm> ultForms = result.getResult();
+//                for (int i = 0;i<ultForms.size();i++) {
+//                    UltForm saveUltForm = JSON.parseObject(JSON.toJSONString(ultForms.get(i)),UltForm.class);
+//                    formBoMapLocalStore.save(saveUltForm);
+//                }
+//            }
+//            return result;
+//        }catch (Exception e){
+//            result.setCode("400");
+//            result.setMessage("部署失败");
+//            return result;
+//        }
+    }
+
+    /**
+     * 根据表单id获取详细json配置
+     * @return
+     */
+    @GetMapping("/form-settings/{id}" )
+    public Response pageBoSeetings(HttpServletRequest request,@PathVariable String id) {
+        DataSet ds = null;
+        String tenantId = request.getParameter("tenantId");
+        Response<UltForm> response = new Response<>();
+        if(!StringUtils.isEmpty(id)) {
+            ResponseList<UltForm> items = getSeetings(id,tenantId);
+            if (items.size() == 1){
+                response.setMessage("查询成功");
+                response.setCode("200");
+                response.setResult(items.get(0));
+                return response;
+            }else {
+                Response<List<UltForm>> result = initSeetings(id);
+                if (result.getResult().size() > 0){
+                    items = getSeetings(id,tenantId);
+                }
+                if (items.size() == 1){
+                    response.setMessage("查询成功");
+                    response.setCode("200");
+                    response.setResult(items.get(0));
+                    return response;
+                }else {
+                    response.setMessage("菜单未部署");
+                    response.setCode("500");
+                    return response;
+                }
+            }
+
+//            Response<UltForm> response = new Response<>();
+//            List<Row> trows = new ArrayList<>();
+//            if (!StringUtils.isEmpty(tenantId)) {
+//                ds = formBoMapLocalStore.query().selectAll()
+//                        .where("refFormId")
+//                        .eq(id)
+//                        .and("tenantId")
+//                        .eq(tenantId)
+//                        .execute();
+//                trows = ds.toRows();
+//            }
+//            if (ds!=null && trows!=null && trows.size() > 0){
+//                ResponseList<UltForm> items = trows.stream().
+//                        map(this::toUltForm).collect(Collectors.toCollection(ResponseList::new));
+//                response.setMessage("查询成功");
+//                response.setCode("1");
+//                if (items.size() == 1) {
+//                    response.setResult(items.get(0));
+//                }
+//            }else {
+//                ds = formBoMapLocalStore.query().selectAll()
+//                        .where("id")
+//                        .eq(id)
+//                        .execute();
+//
+//                List<Row> rows = ds.toRows();
+//                ResponseList<UltForm> items = rows.stream().
+//                        map(this::toUltForm).collect(Collectors.toCollection(ResponseList::new));
+//
+//                response.setMessage("查询成功");
+//                response.setCode("1");
+//                if (items.size() == 1) {
+//                    response.setResult(items.get(0));
+//                }else {
+//                    Response<List<UltForm>> result = initSeetings(id);
+//                    if (result.getResult().size() > 0) {
+//
+//                    }
+//                }
+//            }
+//            return response;
+//
+        }else {
+            response.setMessage("未传id");
+            response.setCode("1");
+            return response;
+        }
+    }
+
+    private Response initSeetings(String id){
         String accessUri = "http://pfcp.phoenix-t.xforceplus.com";
         String url = String.format("%s/forms/%s/deployments"
                 , accessUri
@@ -67,21 +179,14 @@ public class UltFormSettingController {
             return result;
         }catch (Exception e){
             result.setCode("400");
-            result.setMessage("部署失败");
+            result.setMessage("获取失败");
             return result;
         }
     }
 
-    /**
-     * 根据表单id获取详细json配置
-     * @return
-     */
-    @GetMapping("/form-settings/{id}" )
-    public Response pageBoSeetings(HttpServletRequest request,@PathVariable String id) {
+    private ResponseList getSeetings(String id,String tenantId){
         DataSet ds = null;
-        String tenantId = request.getParameter("tenantId");
         if(!StringUtils.isEmpty(id)) {
-            Response<UltForm> response = new Response<>();
             List<Row> trows = new ArrayList<>();
             if (!StringUtils.isEmpty(tenantId)) {
                 ds = formBoMapLocalStore.query().selectAll()
@@ -95,36 +200,19 @@ public class UltFormSettingController {
             if (ds!=null && trows!=null && trows.size() > 0){
                 ResponseList<UltForm> items = trows.stream().
                         map(this::toUltForm).collect(Collectors.toCollection(ResponseList::new));
-                response.setMessage("查询成功");
-                response.setCode("1");
-                if (items.size() == 1) {
-                    response.setResult(items.get(0));
-                }
+                return items;
             }else {
                 ds = formBoMapLocalStore.query().selectAll()
                         .where("id")
                         .eq(id)
                         .execute();
-
                 List<Row> rows = ds.toRows();
                 ResponseList<UltForm> items = rows.stream().
                         map(this::toUltForm).collect(Collectors.toCollection(ResponseList::new));
-
-                response.setMessage("查询成功");
-                response.setCode("1");
-                if (items.size() == 1) {
-                    response.setResult(items.get(0));
-                }
+                return items;
             }
-            return response;
-
         }else {
-            Response<ResponseList<UltPage>> response = new Response<>();
-
-            response.setMessage("未传id");
-            response.setCode("1");
-
-            return response;
+            return null;
         }
     }
 
