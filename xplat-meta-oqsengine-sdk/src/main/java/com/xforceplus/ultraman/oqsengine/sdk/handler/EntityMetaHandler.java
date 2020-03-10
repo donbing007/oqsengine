d56b7c8b548a6b1500f5fbcf0888c2fc0cdfb16f
unknown <wangzheng@xforceplus.com>
2020-03-10 14:37:50 +0800

    更新注解，pom的上传私服id错误，两个默认数据处理器

---
+++ xplat-meta-oqsengine-sdk/src/main/java/com/xforceplus/ultraman/oqsengine/sdk/handler/EntityMetaHandler.java
@@ -0,0 +1,102 @@
+package com.xforceplus.ultraman.oqsengine.sdk.handler;
+
+import com.xforceplus.ultraman.oqsengine.pojo.dto.entity.impl.EntityClass;
+
+import java.util.Map;
+
+/**
+ * Entity公共字段数据填充处理类
+ *
+ * @author wangzheng
+ * @since 2020-03-05
+ */
+public class EntityMetaHandler {
+
+    /**
+     * 保存对象字段填充
+     * @param entityClass 对象
+     * @param body 数据对象
+     * @return body
+     */
+    public Map<String, Object> insertFill(EntityClass entityClass, Map<String, Object> body){
+
+        return body;
+    };
+
+    /**
+     * 更新对象字段填充
+     * @param entityClass
+     * @param body
+     * @return body
+     */
+    public Map<String, Object> updateFill(EntityClass entityClass, Map<String, Object> body){
+
+        return body;
+    };
+
+    /**
+     * 删除对象字段填充
+     * @param entityClass
+     * @param body
+     * @return body
+     */
+    public Map<String, Object> deleteFill(EntityClass entityClass, Map<String, Object> body){
+
+        return body;
+    };
+
+    /**
+     * 判断是否可以填充数据
+     * @param entityClass
+     * @param fieldName
+     * @param fieldVal
+     * @return
+     */
+    public boolean isFill(EntityClass entityClass,String fieldName,Object fieldVal){
+
+        return false;
+    }
+
+    /**
+     * 根据字段名字获取该字段的值
+     * @param entityClass
+     * @param body
+     * @param fieldName
+     * @return
+     */
+    public Object getFieldValByName(EntityClass entityClass, Map<String, Object> body,String fieldName) {
+        if (body.size() == 0 ) {
+            return null;
+        }
+        body.entrySet().stream().map(entry -> {
+            if (entry.getKey().equals(fieldName)){
+                if (isFill(entityClass,fieldName,entry.getValue())){
+                    return entry.getValue();
+                }
+            }
+            return null;
+        });
+        return null;
+    }
+
+    /**
+     * 设置该字段的值
+     * @param entityClass
+     * @param body
+     * @param fieldName
+     * @param fieldVal
+     * @return
+     */
+    public Map<String, Object> setFieldValByName(EntityClass entityClass, Map<String, Object> body,String fieldName, Object fieldVal) {
+        body.entrySet().stream().map(entry -> {
+            if (entry.getKey().equals(fieldName)){
+                if (isFill(entityClass,fieldName,entry.getValue())){
+                    return entry.getValue();
+                }
+            }
+            return null;
+        });
+        return body;
+    }
+
+}
