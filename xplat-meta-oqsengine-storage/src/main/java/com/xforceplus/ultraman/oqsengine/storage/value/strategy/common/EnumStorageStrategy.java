2487e1b519b7cfa64e42a1b2abadd18d05a1fd15
董斌 <dongbing@xforceplus.com>
2020-03-11 18:38:10 +0800

    1. 修正没有正确返回逻辑值类型的问题.

--- xplat-meta-oqsengine-storage/src/main/java/com/xforceplus/ultraman/oqsengine/storage/value/strategy/common/EnumStorageStrategy.java
+++ xplat-meta-oqsengine-storage/src/main/java/com/xforceplus/ultraman/oqsengine/storage/value/strategy/common/EnumStorageStrategy.java
@@ -1,6 +1,10 @@
 package com.xforceplus.ultraman.oqsengine.storage.value.strategy.common;
 
 import com.xforceplus.ultraman.oqsengine.pojo.dto.entity.FieldType;
+import com.xforceplus.ultraman.oqsengine.pojo.dto.entity.IEntityField;
+import com.xforceplus.ultraman.oqsengine.pojo.dto.values.EnumValue;
+import com.xforceplus.ultraman.oqsengine.pojo.dto.values.IValue;
+import com.xforceplus.ultraman.oqsengine.storage.value.StorageValue;
 
 /**
  * 枚举型字段储存策略.
@@ -13,5 +17,10 @@ public class EnumStorageStrategy extends StringStorageStrategy {
     @Override
     public FieldType fieldType() {
         return FieldType.ENUM;
+    }
+
+    @Override
+    public IValue toLogicValue(IEntityField field, StorageValue storageValue) {
+        return new EnumValue(field, (String) storageValue.value());
     }
 }
