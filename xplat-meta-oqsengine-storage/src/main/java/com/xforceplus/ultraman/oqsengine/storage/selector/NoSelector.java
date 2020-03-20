502b26a8b60346a84158018fc2799541ed3be1d7
董斌 <dongbing@xforceplus.com>
2020-03-20 18:34:29 +0800

    1. 提供一个实际不选择的选择器,会原样返回 key.用以做为没有配置选择器时的选择.

---
+++ xplat-meta-oqsengine-storage/src/main/java/com/xforceplus/ultraman/oqsengine/storage/selector/NoSelector.java
@@ -0,0 +1,17 @@
+package com.xforceplus.ultraman.oqsengine.storage.selector;
+
+/**
+ * 实际不进行选择.
+ * 此实现的主要目的在于提供一个默认的 Selector 实现.
+ *
+ * @author dongbin
+ * @version 0.1 2020/3/20 18:29
+ * @since 1.8
+ */
+public class NoSelector<V> implements Selector<V> {
+
+    @Override
+    public V select(String key) {
+        return (V) key;
+    }
+}
