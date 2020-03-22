f34f7b6c512f399a1938d09a3117e2e35db08495
luye <luye66@163.com>
2020-03-22 23:39:42 +0800

    add control on controller init

---
+++ xplat-meta-oqsengine-sdk/src/main/java/com/xforceplus/ultraman/oqsengine/sdk/autoconfigurer/DefaultEndpointConfiguration.java
@@ -0,0 +1,35 @@
+package com.xforceplus.ultraman.oqsengine.sdk.autoconfigurer;
+
+import com.xforceplus.ultraman.oqsengine.sdk.controller.*;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+
+@ConditionalOnProperty(value = "xplat.oqsengine.sdk.endpoint.enabled", matchIfMissing = true)
+public class DefaultEndpointConfiguration {
+
+    //controller
+    @Bean
+    public ModuleController moduleController(){
+        return new ModuleController();
+    }
+
+    @Bean
+    public EntityController entityController(){
+        return new EntityController();
+    }
+
+    @Bean
+    public DictController dictController(){
+        return new DictController();
+    }
+
+    @Bean
+    public UltFormSettingController ultFormSettingController(){
+        return new UltFormSettingController();
+    }
+
+    @Bean
+    public UltPageSettingController ultPageSettingController(){
+        return new UltPageSettingController();
+    }
+}
