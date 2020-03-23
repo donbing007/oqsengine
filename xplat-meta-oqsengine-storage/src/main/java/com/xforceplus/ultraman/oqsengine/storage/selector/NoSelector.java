8d9cd8f4890b7a9dece0a1abe0e65724fefb343c
董斌 <dongbing@xforceplus.com>
2020-03-23 15:00:58 +0800

    1. 修正 checkstyle.

--- xplat-meta-oqsengine-storage/src/main/java/com/xforceplus/ultraman/oqsengine/storage/selector/NoSelector.java
+++ xplat-meta-oqsengine-storage/src/main/java/com/xforceplus/ultraman/oqsengine/storage/selector/NoSelector.java
@@ -4,6 +4,7 @@ package com.xforceplus.ultraman.oqsengine.storage.selector;
  * 实际不进行选择.
  * 此实现的主要目的在于提供一个默认的 Selector 实现.
  *
+ * @param <V> 选择元素类型.
  * @author dongbin
  * @version 0.1 2020/3/20 18:29
  * @since 1.8
