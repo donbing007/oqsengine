4cbf5e0231e8452712cb2b42ef128f1cca0c8a45
luye <luye66@163.com>
2020-06-15 13:41:37 +0800

    add config

---
+++ xplat-meta-oqsengine-sdk/src/main/java/com/xforceplus/ultraman/oqsengine/sdk/config/engine/VersionedJsonConfig.java
@@ -0,0 +1,30 @@
+package com.xforceplus.ultraman.oqsengine.sdk.config.engine;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.xforceplus.ultraman.config.Versioned;
+import com.xforceplus.ultraman.config.json.impl.DefaultJsonConfigNode;
+
+import java.util.Optional;
+
+public class VersionedJsonConfig extends DefaultJsonConfigNode implements Versioned {
+
+    private String version;
+
+    private Integer versionOrder;
+
+    public VersionedJsonConfig(String version, String type, String id, JsonNode jsonNode, Integer versionOrder) {
+        super(id, type, jsonNode);
+        this.version = version;
+        this.versionOrder = versionOrder;
+    }
+
+    @Override
+    public Long getVersion() {
+        return Optional.ofNullable(versionOrder).map(Integer::longValue).orElse(null);
+    }
+
+    @Override
+    public String getVersionStr() {
+        return version;
+    }
+}
