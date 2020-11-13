f2aaccbd904ba4a61150c3e840a884c4d44a9ce6
董斌 <dongbin@xforceplus.com>
2020-11-13 17:06:32 +0800

    1.修正配置错误.

--- xplat-meta-oqsengine-boot/src/main/java/com/xforceplus/ultraman/oqsengine/boot/config/CommonConfiguration.java
+++ xplat-meta-oqsengine-boot/src/main/java/com/xforceplus/ultraman/oqsengine/boot/config/CommonConfiguration.java
@@ -43,15 +43,15 @@ public class CommonConfiguration {
     @ConditionalOnExpression("${storage.index.write.shard.enabled} == true")
     @Bean("indexWriteIndexNameSelector")
     public Selector<String> shardingIndexWriteIndexNameSelector(
-        @Value("{storage.index.write.name:oqsindex}") String baseIndexName,
-        @Value("{storage.index.write.shard.size:1}") int shardSize) {
+        @Value("${storage.index.write.name:oqsindex}") String baseIndexName,
+        @Value("${storage.index.write.shard.size:1}") int shardSize) {
         return new SuffixNumberHashSelector(baseIndexName, shardSize);
     }
 
     @ConditionalOnExpression("${storage.index.write.shard.enabled} == false")
     @Bean("indexWriteIndexNameSelector")
     public Selector<String> noShardingIndexWriteIndexNameSelector(
-        @Value("{storage.index.write.name:oqsindex}") String baseIndexName) {
+        @Value("${storage.index.write.name:oqsindex}") String baseIndexName) {
         return new NoSelector(baseIndexName);
     }
 
