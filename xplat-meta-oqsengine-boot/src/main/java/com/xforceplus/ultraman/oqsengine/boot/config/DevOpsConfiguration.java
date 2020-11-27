c82348f2460c162a43a20bdd88fe348ff0e2441d
徐佳 <xujia@xforceplus.com>
2020-11-27 09:23:22 +0800

    修改configmap

--- xplat-meta-oqsengine-boot/src/main/java/com/xforceplus/ultraman/oqsengine/boot/config/DevOpsConfiguration.java
+++ xplat-meta-oqsengine-boot/src/main/java/com/xforceplus/ultraman/oqsengine/boot/config/DevOpsConfiguration.java
@@ -22,14 +22,14 @@ public class DevOpsConfiguration {
 
     @Bean
     public RebuildIndexExecutor devOpsRebuildIndexExecutor(
-            @Value("${devops.task.split:10}") int splitPart,
-            @Value("${devops.task.max.queue.size:2000}") int maxQueueSize,
-            @Value("${devops.task.execution.timeout:30000}") int executionTimeout,
-            @Value("${devops.task.update.frequency:100}") int updateFrequency,
-            @Value("${devops.task.cache.expire:30}") long cacheExpireTime,
-            @Value("${devops.task.cache.maxsize:500}") long cacheMaxSize,
+            @Value("${storage.devOps.task.split:10}") int splitPart,
+            @Value("${storage.devOps.task.max.queue.size:2000}") int maxQueueSize,
+            @Value("${storage.devOps.task.execution.timeout:30000}") int executionTimeout,
+            @Value("${storage.devOps.task.update.frequency:100}") int updateFrequency,
+            @Value("${storage.devOps.task.cache.expire:30}") long cacheExpireTime,
+            @Value("${storage.devOps.task.cache.maxsize:500}") long cacheMaxSize,
             @Value("${storage.devOps.maxQueryTimeMs:3000}") int taskTimeout,
-            @Value("${storage.task.page.size:1000}") int pageSize) {
+            @Value("${storage.devOps.task.page.size:1000}") int pageSize) {
         return new DevOpsRebuildIndexExecutor(splitPart, maxQueueSize, executionTimeout,
                 updateFrequency, cacheExpireTime, cacheMaxSize, taskTimeout, pageSize);
     }
