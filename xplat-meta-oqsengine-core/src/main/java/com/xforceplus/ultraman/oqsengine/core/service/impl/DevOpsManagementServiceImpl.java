82d2ad6ca996701ca4b4fd59950cec7bd4981f2c
徐佳 <xujia@xforceplus.com>
2020-12-15 16:41:58 +0800

    DevOpsManagementService 增加Cancel测试

--- xplat-meta-oqsengine-core/src/main/java/com/xforceplus/ultraman/oqsengine/core/service/impl/DevOpsManagementServiceImpl.java
+++ xplat-meta-oqsengine-core/src/main/java/com/xforceplus/ultraman/oqsengine/core/service/impl/DevOpsManagementServiceImpl.java
@@ -130,11 +130,8 @@ public class DevOpsManagementServiceImpl implements DevOpsManagementService {
             message = String.format("task %s can not be cancel, taskId is invalid.", taskId);
         }
 
-        if (null != message) {
-            logger.error(message);
-            throw new SQLException(message, VALIDATION_ERROR.name(), VALIDATION_ERROR.ordinal());
-        }
-
+        logger.error(message);
+        throw new SQLException(message, VALIDATION_ERROR.name(), VALIDATION_ERROR.ordinal());
     }
 
     @Override
