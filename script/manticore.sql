2d234dd297687342023754edab05919f208bb8fb
董斌 <dongbin@xforceplus.com>
2021-01-05 14:03:46 +0800

    1. 增加'.'至索引中.原先会忽略掉'.'.

--- script/manticore.sql
+++ script/manticore.sql
@@ -11,4 +11,4 @@ create table oqsindex
     jsonfields json,
     entityf    text indexed,
     fullfields text indexed
-) html_strip='1' index_zones='F*' infix_fields='fullfields' min_infix_len='3' charset_table='U+ff01,U+ff04,U+ff07,U+ff09,U+ff0d,U+ff0f,U+ff1c,U+ff20,U+ff3c,U+ff3e,U+ff5c,U+ff5e,U+ff0a,U+ff02,non_cjk' ngram_chars='cjk' ngram_len='1' rt_mem_limit='1024m';
+) html_strip='1' index_zones='F*' infix_fields='fullfields' min_infix_len='3' charset_table='U+ff01,U+ff04,U+ff07,U+ff09,U+ff0d,U+ff0f,U+ff1c,U+ff20,U+ff3c,U+ff3e,U+ff5c,U+ff5e,U+ff0a,U+ff02,U+ff0e,non_cjk' ngram_chars='cjk' ngram_len='1' rt_mem_limit='1024m';
