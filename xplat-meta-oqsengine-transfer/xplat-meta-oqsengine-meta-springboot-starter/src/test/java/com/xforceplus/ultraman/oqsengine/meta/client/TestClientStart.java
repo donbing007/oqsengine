4f4a0863e93322e877244763d017645d2158f194
徐佳 <xujia@xforceplus.com>
2021-03-04 17:34:23 +0800

    修改proto结构、整合测试base

--- xplat-meta-oqsengine-transfer/xplat-meta-oqsengine-meta-springboot-starter/src/test/java/com/xforceplus/ultraman/oqsengine/meta/client/TestClientStart.java
+++ xplat-meta-oqsengine-transfer/xplat-meta-oqsengine-meta-springboot-starter/src/test/java/com/xforceplus/ultraman/oqsengine/meta/client/TestClientStart.java
@@ -1,14 +1,14 @@
 package com.xforceplus.ultraman.oqsengine.meta.client;
 
 import com.xforceplus.ultraman.oqsengine.meta.SpringBootApp;
+import com.xforceplus.ultraman.oqsengine.meta.common.dto.WatchElement;
 import com.xforceplus.ultraman.oqsengine.meta.common.utils.ThreadUtils;
+import com.xforceplus.ultraman.oqsengine.meta.handler.IRequestHandler;
 import io.grpc.netty.NettyServerBuilder;
 import io.grpc.testing.GrpcCleanupRule;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -28,19 +28,37 @@ import java.io.IOException;
 @SpringBootTest(classes = SpringBootApp.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TestClientStart {
 
-    @Rule
-    public GrpcCleanupRule gRpcCleanup = new GrpcCleanupRule();
+    @Autowired
+    private IRequestHandler requestHandler;
 
     Thread serverThread;
 
+    boolean isLocalTest = true;
+
     @Before
     public void before() throws InterruptedException {
-        buildServer();
+        if (isLocalTest) {
+            buildServer();
+        }
         Thread.sleep(1_000);
     }
     @After
     public void after() throws InterruptedException {
-        ThreadUtils.shutdown(serverThread, 1);
+        if (isLocalTest) {
+            ThreadUtils.shutdown(serverThread, 1);
+
+            Thread.sleep(5_000);
+        }
+    }
+
+    @Test
+    public void test() throws InterruptedException {
+        if (!isLocalTest) {
+            boolean ret =
+                    requestHandler.register(new WatchElement("7", "0", -1, WatchElement.AppStatus.Register));
+
+            Assert.assertTrue(ret);
+        }
 
         Thread.sleep(5_000);
     }
@@ -49,22 +67,17 @@ public class TestClientStart {
         MockServer mockServer = new MockServer();
         try {
             serverThread = ThreadUtils.create(() -> {
-                        try {
-                            NettyServerBuilder
-                                    .forPort(8083).directExecutor().addService(mockServer).build().start();
-                        } catch (IOException e) {
-                            System.exit(-1);
-                        }
-                        return true;
+                try {
+                    NettyServerBuilder
+                            .forPort(8082).directExecutor().addService(mockServer).build().start();
+                } catch (IOException e) {
+                    System.exit(-1);
+                }
+                return true;
             });
             serverThread.start();
         } catch (Exception e) {
             throw new RuntimeException();
         }
-    }
-
-    @Test
-    public void test() throws InterruptedException {
-        Thread.sleep(10_000);
     }
 }
