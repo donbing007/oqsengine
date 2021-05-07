f3d312407959336dc5766b6a9d7a6e3481203d9f
徐佳 <xujia@xforceplus.com>
2021-05-07 14:13:51 +0800

    fixed checkstyle

--- xplat-meta-oqsengine-cdc/src/main/java/com/xforceplus/ultraman/oqsengine/cdc/consumer/impl/SphinxConsumerService.java
+++ xplat-meta-oqsengine-cdc/src/main/java/com/xforceplus/ultraman/oqsengine/cdc/consumer/impl/SphinxConsumerService.java
@@ -204,7 +204,7 @@ public class SphinxConsumerService implements ConsumerService {
                     }
                 }
 
-                pos ++;
+                pos++;
             }
         }
     }
