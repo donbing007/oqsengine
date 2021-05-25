62bc26b8ade4dd41c8739211ebb68e345d81dd56
luye <luye66@163.com>
2021-05-25 18:34:08 +0800

    fix error

--- xplat-meta-oqsengine-index/src/main/java/com/xforceplus/ultraman/oqsengine/storage/index/sphinxql/SphinxQLManticoreIndexStorage.java
+++ xplat-meta-oqsengine-index/src/main/java/com/xforceplus/ultraman/oqsengine/storage/index/sphinxql/SphinxQLManticoreIndexStorage.java
@@ -422,7 +422,7 @@ public class SphinxQLManticoreIndexStorage implements IndexStorage {
             IEntityField field = fieldOp.get();
             StorageValue storageValue =
                 storageStrategyFactory.getStrategy(field.type())
-                    .convertIndexStorageValue(attr.getKey(), attr.getValue());
+                    .convertIndexStorageValue(AnyStorageValue.compatibleStorageName(attr.getKey()), attr.getValue());
 
             if (StorageType.UNKNOWN == anyStorageValue.type()) {
                 if (fieldOp.isPresent()) {
