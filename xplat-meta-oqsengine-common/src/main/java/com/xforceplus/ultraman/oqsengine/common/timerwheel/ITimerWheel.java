a763f5d70ac1f231165d86f54d5aa4140451cbaf
韦凯 <weikai@xforceplus.com>
2021-05-26 17:11:03 +0800

    fix checkStyle

--- xplat-meta-oqsengine-common/src/main/java/com/xforceplus/ultraman/oqsengine/common/timerwheel/ITimerWheel.java
+++ xplat-meta-oqsengine-common/src/main/java/com/xforceplus/ultraman/oqsengine/common/timerwheel/ITimerWheel.java
@@ -1,21 +1,27 @@
 package com.xforceplus.ultraman.oqsengine.common.timerwheel;
 
 /**
+ * 时间轮接口.
+ *
  * @param <T> 管理的元素类型.
  * @author weikai
- * @data 2021/5/21 15:50
- * @mail weikai@xforceplus.com
+ * @version 1.0 2021/5/21 15:50
+ * @since 1.5
  */
-public interface ITimerWheel<T>{
+public interface ITimerWheel<T> {
 
     /**
-     * @param transaction
-     * @param timeoutMs
+     * 添加任务.
+     *
+     * @param transaction 超时通知
+     * @param timeoutMs 超时时间
      */
     void add(T transaction, long timeoutMs);
 
     /**
-     * @param tx
+     * 删除任务.
+     *
+     * @param tx 待删除任务
      */
     void remove(T tx);
 }
