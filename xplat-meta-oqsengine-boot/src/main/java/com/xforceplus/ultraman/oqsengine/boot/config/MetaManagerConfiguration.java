f9de3250f836939660b2a2932d3414d27580006d
徐佳 <xujia@xforceplus.com>
2021-06-17 16:21:37 +0800

    增加rest运维接口，可将Meta数据导入到OQS中

--- xplat-meta-oqsengine-boot/src/main/java/com/xforceplus/ultraman/oqsengine/boot/config/MetaManagerConfiguration.java
+++ xplat-meta-oqsengine-boot/src/main/java/com/xforceplus/ultraman/oqsengine/boot/config/MetaManagerConfiguration.java
@@ -1,6 +1,9 @@
 package com.xforceplus.ultraman.oqsengine.boot.config;
 
+import com.google.protobuf.InvalidProtocolBufferException;
 import com.xforceplus.ultraman.oqsengine.meta.common.executor.IDelayTaskExecutor;
+import com.xforceplus.ultraman.oqsengine.meta.common.proto.sync.EntityClassSyncRspProto;
+import com.xforceplus.ultraman.oqsengine.meta.common.utils.EntityClassStorageHelper;
 import com.xforceplus.ultraman.oqsengine.meta.provider.outter.SyncExecutor;
 import com.xforceplus.ultraman.oqsengine.metadata.MetaManager;
 import com.xforceplus.ultraman.oqsengine.metadata.StorageMetaManager;
@@ -8,6 +11,8 @@ import com.xforceplus.ultraman.oqsengine.metadata.cache.CacheExecutor;
 import com.xforceplus.ultraman.oqsengine.metadata.cache.DefaultCacheExecutor;
 import com.xforceplus.ultraman.oqsengine.metadata.executor.EntityClassSyncExecutor;
 import com.xforceplus.ultraman.oqsengine.metadata.executor.ExpireExecutor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +22,8 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class MetaManagerConfiguration {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Bean("metaManager")
     @ConditionalOnExpression("'${meta.grpc.type}'.equals('client') || '${meta.grpc.type}'.equals('server')")
@@ -34,6 +41,36 @@ public class MetaManagerConfiguration {
     @ConditionalOnExpression("'${meta.grpc.type}'.equals('client') || '${meta.grpc.type}'.equals('server')")
     public SyncExecutor grpcSyncExecutor() {
         return new EntityClassSyncExecutor();
+    }
+
+    /**
+     * 产生一个mock的syncExecutor.
+     */
+    @Bean
+    @ConditionalOnExpression("'${meta.grpc.type}'.equals('mock')")
+    public SyncExecutor mockSyncExecutor() {
+        return new SyncExecutor() {
+            @Override
+            public boolean sync(String appId, int version, EntityClassSyncRspProto entityClassSyncRspProto) {
+                return true;
+            }
+
+            @Override
+            public boolean dataImport(String appId, int version, String content) {
+                try {
+                    EntityClassStorageHelper.toEntityClassSyncRspProto(content);
+                    return true;
+                } catch (InvalidProtocolBufferException e) {
+                    logger.error("toEntityClassSyncRspProto error, message {}", e.getMessage());
+                    return false;
+                }
+            }
+
+            @Override
+            public int version(String appId) {
+                return 0;
+            }
+        };
     }
 
     @Bean
