1b16e4a6b59c9b5dc677ab8b139bebb0b8734fec
徐佳 <xujia@xforceplus.com>
2021-06-22 15:10:52 +0800

    增加在offLine模式下不启动requestHandler

--- xplat-meta-oqsengine-metadata/src/main/java/com/xforceplus/ultraman/oqsengine/metadata/utils/FileReaderUtils.java
+++ xplat-meta-oqsengine-metadata/src/main/java/com/xforceplus/ultraman/oqsengine/metadata/utils/FileReaderUtils.java
@@ -16,14 +16,18 @@ public class FileReaderUtils {
     public static List<String> getFileNamesInOneDir(String path) {
 
         List<String> files = new ArrayList<>();
-        File file = new File(path);
-        File[] tempList = file.listFiles();
-        if (null != tempList) {
-            for (int i = 0; i < tempList.length; i++) {
-                if (tempList[i].isFile()) {
-                    files.add(tempList[i].getName());
+        try {
+            File file = new File(path);
+            File[] tempList = file.listFiles();
+            if (null != tempList) {
+                for (int i = 0; i < tempList.length; i++) {
+                    if (tempList[i].isFile()) {
+                        files.add(tempList[i].getName());
+                    }
                 }
             }
+        } catch (Exception e) {
+            //  ignore...
         }
         return files;
     }
