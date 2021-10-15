8395c69164352205655b3956bf8652004c0cb1cf
董斌 <dongbin@xforceplus.com>
2021-10-15 16:50:42 +0800

    1. 按照新的logic规范修改公式和自增编号的实现.

--- xplat-meta-oqsengine-calculate/src/main/java/com/xforceplus/ultraman/oqsengine/calculation/logic/formula/FormulaCalculationLogic.java
+++ xplat-meta-oqsengine-calculate/src/main/java/com/xforceplus/ultraman/oqsengine/calculation/logic/formula/FormulaCalculationLogic.java
@@ -2,13 +2,12 @@ package com.xforceplus.ultraman.oqsengine.calculation.logic.formula;
 
 import com.xforceplus.ultraman.oqsengine.calculation.context.CalculationContext;
 import com.xforceplus.ultraman.oqsengine.calculation.exception.CalculationException;
+import com.xforceplus.ultraman.oqsengine.calculation.helper.FormulaHelper;
 import com.xforceplus.ultraman.oqsengine.calculation.logic.CalculationLogic;
-import com.xforceplus.ultraman.oqsengine.calculation.utils.infuence.Infuence;
-import com.xforceplus.ultraman.oqsengine.calculation.utils.infuence.Participant;
 import com.xforceplus.ultraman.oqsengine.pojo.dto.entity.CalculationType;
-import com.xforceplus.ultraman.oqsengine.pojo.dto.entity.IEntity;
+import com.xforceplus.ultraman.oqsengine.pojo.dto.entity.impl.calculation.Formula;
 import com.xforceplus.ultraman.oqsengine.pojo.dto.values.IValue;
-import java.util.Collection;
+import com.xforceplus.ultraman.oqsengine.pojo.utils.IValueUtils;
 import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,51 +23,36 @@ public class FormulaCalculationLogic implements CalculationLogic {
 
     private static final int MAX_ERROR_MESSAGE_LENGTH = 256;
 
-//    @Override
-//    public Optional<IValue> calculate(CalculationLogicContext context) throws CalculationException {
-//
-//        Formula formula = (Formula) context.getFocusField().config().getCalculation();
-//
-//        //  执行公式
-//        try {
-//            //  调用公式执行器执行
-//            return Optional.of(IValueUtils.toIValue(context.getFocusField(),
-//                FormulaHelper.calculate(formula.getExpression(), formula.getArgs(), context)));
-//        } catch (Exception e) {
-//            //  异常时
-//            if (formula.getFailedPolicy().equals(Formula.FailedPolicy.USE_FAILED_DEFAULT_VALUE)) {
-//                try {
-//                    logger.warn(
-//                        "formula [entityFieldId-{}] has executed failed, will use failed default value to instead, [reason-{}]",
-//                        context.getFocusField().id(), e.getMessage());
-//
-//                    return Optional.of(IValueUtils.toIValue(context.getFocusField(), formula.getFailedDefaultValue()));
-//                } finally {
-//                    //  将错误加入hints中
-//                    context.hint(e.getMessage().substring(0, Math.min(e.getMessage().length(), MAX_ERROR_MESSAGE_LENGTH)));
-//                }
-//            } else {
-//                logger.warn("formula [entityFieldId-{}] has executed failed, execution will broken, [reason-{}]",
-//                    context.getFocusField().id(), e.getMessage());
-//                throw new CalculationException(e.getMessage(), e);
-//            }
-//        }
-//    }
-
     @Override
     public Optional<IValue> calculate(CalculationContext context) throws CalculationException {
-        return Optional.empty();
-    }
+        Formula formula = (Formula) context.getFocusField().config().getCalculation();
 
-    @Override
-    public void scope(CalculationContext context, Infuence infuence) {
+        //  执行公式
+        try {
+            //  调用公式执行器执行
+            return Optional.of(IValueUtils.toIValue(context.getFocusField(),
+                FormulaHelper.calculate(formula.getExpression(), formula.getArgs(), context)));
+        } catch (Exception e) {
+            //  异常时
+            if (formula.getFailedPolicy().equals(Formula.FailedPolicy.USE_FAILED_DEFAULT_VALUE)) {
+                try {
+                    logger.warn(
+                        "formula [entityFieldId-{}] has executed failed, will use failed default value to instead, [reason-{}]",
+                        context.getFocusField().id(), e.getMessage());
 
-    }
-
-    @Override
-    public long[] getMaintainTarget(CalculationContext context, Participant participant, Collection<IEntity> entities)
-        throws CalculationException {
-        return new long[0];
+                    return Optional.of(IValueUtils.toIValue(context.getFocusField(), formula.getFailedDefaultValue()));
+                } finally {
+                    //  将错误加入hints中
+                    context.hint(
+                        context.getFocusField(),
+                        e.getMessage().substring(0, Math.min(e.getMessage().length(), MAX_ERROR_MESSAGE_LENGTH)));
+                }
+            } else {
+                logger.warn("formula [entityFieldId-{}] has executed failed, execution will broken, [reason-{}]",
+                    context.getFocusField().id(), e.getMessage());
+                throw new CalculationException(e.getMessage(), e);
+            }
+        }
     }
 
     @Override
