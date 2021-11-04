cdac9deb31acdbabb457eb9c953e0fe41d053066
韦凯 <weikai@xforceplus.com>
2021-11-04 21:46:51 +0800

    还原配置

--- xplat-meta-oqsengine-metadata/src/main/java/com/xforceplus/ultraman/oqsengine/metadata/cache/AggregationEventBuilder.java
+++ xplat-meta-oqsengine-metadata/src/main/java/com/xforceplus/ultraman/oqsengine/metadata/cache/AggregationEventBuilder.java
@@ -20,7 +20,6 @@ import java.util.stream.Collectors;
 import javax.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 
 
 /**
