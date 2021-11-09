5eaf88fb90109596e36ca3e76eb107acf43f5c26
董斌 <dongbin@xforceplus.com>
2021-11-09 17:46:28 +0800

    1. 修正kv表结构错误.

--- ci.sh
+++
@@ -1,3 +0,0 @@
-uuid=$(dbus-uuidgen)
-echo "generate uuid is ${uuid}"
-mvn clean install -U -s ./.mvn/wrapper/settings.xml -Drequest.uuid=${uuid} -Dcontainer.server.port=9898
--- xplat-meta-oqsengine-task/src/test/resources/mysql/mastdb.sql
+++ xplat-meta-oqsengine-task/src/test/resources/mysql/mastdb.sql
@@ -1,7 +1,7 @@
 use oqsengine;
 create table kv (
-  k       varchar(255)          not null comment 'key',
-  v     blob                           comment '值',
-  primary key (k)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
-
+  k  varchar(255)          not null comment 'key',
+  h  bigint                not null comment 'key的哈希值,主要用以分区',
+  v  blob                           comment '值',
+  unique key unique_key(k, h)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 PARTITION BY HASH(h) PARTITIONS 40;
