ee1c3a80d9d92bfffb076bd7168ced425782c3ac
董斌 <dongbin@xforceplus.com>
2021-11-16 17:32:22 +0800

    1. 修正错误.

--- xplat-meta-oqsengine-transfer/xplat-meta-oqsengine-meta-client/src/main/java/com/xforceplus/ultraman/oqsengine/meta/provider/outter/SyncExecutor.java
+++ xplat-meta-oqsengine-transfer/xplat-meta-oqsengine-meta-client/src/main/java/com/xforceplus/ultraman/oqsengine/meta/provider/outter/SyncExecutor.java
@@ -19,8 +19,6 @@ public interface SyncExecutor {
      */
     boolean sync(String appId, int version, EntityClassSyncRspProto entityClassSyncRspProto);
 
-    boolean dataImport(String appId, int version, String content);
-
     /**
      * 外部实现获取版本.
      */
