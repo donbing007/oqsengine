45daafb55b69be646b6742289ddbea5888d15c02
徐佳 <xujia@xforceplus.com>
2021-12-02 17:22:04 +0800

    update metrics

--- xplat-meta-oqsengine-cdc/src/main/java/com/xforceplus/ultraman/oqsengine/cdc/consumer/tools/BinLogParseUtils.java
+++ xplat-meta-oqsengine-cdc/src/main/java/com/xforceplus/ultraman/oqsengine/cdc/consumer/tools/BinLogParseUtils.java
@@ -87,11 +87,7 @@ public class BinLogParseUtils {
      */
     public static String getStringWithoutNullCheck(List<CanalEntry.Column> columns,
                                                    OqsBigEntityColumns oqsBigEntityColumns) {
-        CanalEntry.Column column = existsColumn(columns, oqsBigEntityColumns);
-        if (null != column && !column.getValue().isEmpty()) {
-            return column.getValue();
-        }
-        return null;
+        return columns.get(oqsBigEntityColumns.ordinal()).getValue();
     }
 
     /**
@@ -99,36 +95,6 @@ public class BinLogParseUtils {
      */
     public static CanalEntry.Column getColumnWithoutNull(List<CanalEntry.Column> columns,
                                                          OqsBigEntityColumns oqsBigEntityColumns) throws SQLException {
-        CanalEntry.Column column = existsColumn(columns, oqsBigEntityColumns);
-        if (null == column || column.getValue().isEmpty()) {
-            throw new SQLException(String.format("%s must not be null.", oqsBigEntityColumns.name()));
-        }
-        return column;
+        return columns.get(oqsBigEntityColumns.ordinal());
     }
-
-    /**
-     * 判断是否存在字段.
-     */
-    public static CanalEntry.Column existsColumn(List<CanalEntry.Column> columns, OqsBigEntityColumns compare) {
-        CanalEntry.Column column = null;
-        try {
-            //  通过下标找一次，如果名字相同，则返回当前column
-            column = columns.get(compare.ordinal());
-            if (column.getName().toLowerCase().equals(compare.name().toLowerCase())) {
-                return column;
-            }
-        } catch (Exception e) {
-            //  out of band, logger error?
-        }
-
-        //  binlog记录在columns中顺序不对，需要遍历再找一次(通过名字)
-        for (CanalEntry.Column value : columns) {
-            if (compare.name().toLowerCase().equals(value.getName().toLowerCase())) {
-                return value;
-            }
-        }
-
-        return null;
-    }
-
 }
