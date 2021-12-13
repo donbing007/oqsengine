888226fe5580730c9de0e036ea443f3fb0f95314
董斌 <dongbin@xforceplus.com>
2021-12-13 16:06:31 +0800

    1. 诊断信息.

--- xplat-meta-oqsengine-boot/src/main/java/com/xforceplus/ultraman/oqsengine/boot/OqsengineBootApplication.java
+++ xplat-meta-oqsengine-boot/src/main/java/com/xforceplus/ultraman/oqsengine/boot/OqsengineBootApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
@@ -17,6 +17,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
     DataSourceAutoConfiguration.class,
     DataSourceTransactionManagerAutoConfiguration.class,
     HibernateJpaAutoConfiguration.class})
+@ComponentScan("com.xforceplus.ultraman.oqsengine.meta.config")
 public class OqsengineBootApplication {
 
     public static void main(String[] args) throws Exception {
