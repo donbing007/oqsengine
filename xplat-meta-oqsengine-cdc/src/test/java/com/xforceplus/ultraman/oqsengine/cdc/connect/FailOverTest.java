9eae28137edd2e36e84f25da57b49d8e08fa52d2
董斌 <dongbin@xforceplus.com>
2022-02-11 14:21:06 +0800

    1. 修改测试.

--- xplat-meta-oqsengine-cdc/src/test/java/com/xforceplus/ultraman/oqsengine/cdc/connect/FailOverTest.java
+++ xplat-meta-oqsengine-cdc/src/test/java/com/xforceplus/ultraman/oqsengine/cdc/connect/FailOverTest.java
@@ -5,12 +5,14 @@ import static com.xforceplus.ultraman.oqsengine.cdc.EntityClassBuilder.getEntity
 import com.xforceplus.ultraman.oqsengine.cdc.AbstractCDCTestHelper;
 import com.xforceplus.ultraman.oqsengine.cdc.CDCDaemonService;
 import com.xforceplus.ultraman.oqsengine.cdc.EntityGenerateToolBar;
+import com.xforceplus.ultraman.oqsengine.common.pool.ExecutorHelper;
 import com.xforceplus.ultraman.oqsengine.pojo.dto.entity.IEntity;
 import com.xforceplus.ultraman.oqsengine.storage.master.mock.MasterDBInitialization;
 import com.xforceplus.ultraman.oqsengine.storage.mock.StorageInitialization;
 import com.xforceplus.ultraman.oqsengine.storage.transaction.Transaction;
 import com.xforceplus.ultraman.oqsengine.storage.transaction.TransactionManager;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -18,6 +20,8 @@ import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
@@ -28,6 +32,9 @@ import org.junit.jupiter.api.Test;
  * @since : 1.8
  */
 public class FailOverTest extends AbstractCDCTestHelper {
+
+    static final Logger LOGGER = LoggerFactory.getLogger(FailOverTest.class);
+
     private static CDCDaemonService cdcDaemonService;
 
     private static final int PARTITION = 2000000;
@@ -52,67 +59,93 @@ public class FailOverTest extends AbstractCDCTestHelper {
         ExecutorService executorService = new ThreadPoolExecutor(2, 2, 0,
             TimeUnit.SECONDS, new LinkedBlockingDeque<>(50));
 
-        executorService.submit(new MysqlInitCall());
-        executorService.submit(new CDCDamonServiceCall());
+        CountDownLatch latch = new CountDownLatch(2);
+        executorService.submit(new MysqlInitCall(latch));
+        executorService.submit(new CDCDamonServiceCall(latch));
 
         //  睡眠120秒，结束
         Thread.sleep(100_000);
         System.out.println("stop");
         isTetOver = true;
-        //  继续等待10秒，结束
-        Thread.sleep(10_000);
+
+        latch.await(10, TimeUnit.SECONDS);
+
+        ExecutorHelper.shutdownAndAwaitTermination(executorService);
     }
 
     //  模拟5秒宕机, 5秒后恢复
     private class CDCDamonServiceCall implements Callable {
+
+        private CountDownLatch latch;
+
+        public CDCDamonServiceCall(CountDownLatch latch) {
+            this.latch = latch;
+        }
+
         @Override
         public Object call() throws Exception {
             System.out.println("start CDCDamonServiceCall thread.");
-            while (!isTetOver) {
-                cdcDaemonService.init();
+            try {
+                while (!isTetOver) {
+                    cdcDaemonService.init();
 
-                Thread.sleep(20_000);
+                    Thread.sleep(20_000);
 
-                cdcDaemonService.destroy();
+                    cdcDaemonService.destroy();
 
-                Thread.sleep(5_000);
+                    Thread.sleep(5_000);
+                }
+            } finally {
+                latch.countDown();
             }
             System.out.println("stop CDCDamonServiceCall thread.");
+
             return null;
         }
     }
 
     private class MysqlInitCall implements Callable {
 
+        private CountDownLatch latch;
+
+        public MysqlInitCall(CountDownLatch latch) {
+            this.latch = latch;
+        }
+
         @Override
         public Object call() throws Exception {
             System.out.println("start MysqlInitCall thread.");
             int i = PARTITION;
-            while (!isTetOver) {
-                TransactionManager transactionManager = StorageInitialization.getInstance().getTransactionManager();
-                Transaction tx = transactionManager.create();
-                transactionManager.bind(tx.id());
-                try {
-                    int j = 0;
-                    while (j < MAX) {
-                        initData(EntityGenerateToolBar.generateFixedEntities(i + j * 10, 0), false);
-                        j++;
+            try {
+                while (!isTetOver) {
+                    TransactionManager transactionManager = StorageInitialization.getInstance().getTransactionManager();
+                    Transaction tx = transactionManager.create();
+                    transactionManager.bind(tx.id());
+                    try {
+                        int j = 0;
+                        while (j < MAX) {
+                            initData(EntityGenerateToolBar.generateFixedEntities(i + j * 10, 0), false);
+                            j++;
+                        }
+                        j = 0;
+                        while (j < MAX) {
+                            initData(EntityGenerateToolBar.generateFixedEntities(i + j * 10, 1), true);
+                            j++;
+                        }
+                    } catch (Exception ex) {
+                        LOGGER.error(ex.getMessage(), ex);
+                        tx.rollback();
+                        throw ex;
                     }
-                    j = 0;
-                    while (j < MAX) {
-                        initData(EntityGenerateToolBar.generateFixedEntities(i + j * 10, 1), true);
-                        j++;
-                    }
-                } catch (Exception ex) {
-                    tx.rollback();
-                    throw ex;
+                    transactionManager.getCurrent().get().commit();
+                    transactionManager.finish();
+
+                    i += 1200 + 10;
+
+                    Thread.sleep(5_000);
                 }
-                tx.commit();
-                transactionManager.finish();
-
-                i += 1200 + 10;
-
-                Thread.sleep(5_000);
+            } finally {
+                latch.countDown();
             }
             System.out.println("stop MysqlInitCall thread.");
             return null;
