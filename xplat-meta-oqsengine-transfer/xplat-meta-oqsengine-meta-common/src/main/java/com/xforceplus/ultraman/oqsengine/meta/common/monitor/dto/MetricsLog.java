59c59da07dddd075b1a798f77f15766ad009366d
徐佳 <xujia@xforceplus.com>
2022-03-31 16:22:39 +0800

    修正bug，metaload的时候老数据没有appCode会报错的问题

--- xplat-meta-oqsengine-transfer/xplat-meta-oqsengine-meta-common/src/main/java/com/xforceplus/ultraman/oqsengine/meta/common/monitor/dto/MetricsLog.java
+++ xplat-meta-oqsengine-transfer/xplat-meta-oqsengine-meta-common/src/main/java/com/xforceplus/ultraman/oqsengine/meta/common/monitor/dto/MetricsLog.java
@@ -102,17 +102,17 @@ public class MetricsLog {
         raw.forEach(
             (key, value) -> {
                 value.forEach(
-                    (code, error) -> {
+                    (appId, error) -> {
                         MetricsLog metricsLog;
-                        if (code.contains(MetricsLog.LINK)) {
-                            String[] sp = code.split(MetricsLog.LINK);
+                        if (key.contains(MetricsLog.LINK)) {
+                            String[] sp = appId.split(MetricsLog.LINK);
                             if (sp.length > 1) {
-                                metricsLog = new MetricsLog(key, sp[0], sp[1], error);
+                                metricsLog = new MetricsLog(appId, sp[0], sp[1], error);
                             } else {
-                                metricsLog = new MetricsLog(key, code, error);
+                                metricsLog = new MetricsLog(appId, key, error);
                             }
                         }  else {
-                            metricsLog = new MetricsLog(key, code, error);
+                            metricsLog = new MetricsLog(appId, key, error);
                         }
                         metricsLogs.add(metricsLog);
                     }
