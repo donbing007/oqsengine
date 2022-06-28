7fd8bddc520272da1e84c2093d3bc208739c4e9a
董斌 <dongbin@xforceplus.com>
2022-06-28 15:39:37 +0800

    build(*): 增加了提交日志规范的检查.
    现在定义了提交时的日志规范检查git
    hooks.
    规范定义是"https://docs.google.com/document/d/1QrDFcIiPjSLDn3EL15IJygNPiHORgU1_OOAqWjiDU5Y/edit#"

--- script/git/validate-commit-message.sh
+++ script/git/validate-commit-message.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 # Just for commit-msg hook
-# hook.version@1.0.1
-
-COLOR_RED='\033[0;31m'
-COLOR_NONE='\033[0m'
 
 # 提交日志类型
 COMMIT_TYPE_ARRAR=("feat" "fix" "docs" "style" "refactor" "test" "chore" "build" "ci" "revert")
 
 # 帮助文档
 refer_commit_guide='https://docs.google.com/document/d/1QrDFcIiPjSLDn3EL15IJygNPiHORgU1_OOAqWjiDU5Y/edit#'
+plugin="[IDEA: Git commit template, vscode: git-commit-plugin]"
 
-commit_msg=$(cat "$1" |grep -v "^[#,;]")
+commit_msg=$(cat "$git_message_file" |grep -v "^[#,;]")
+echo "Your Commit message is: $commit_msg"
 commit_line_num=$(echo "$commit_msg" | wc -l) # grep -v "^$"
 commit_msg_head=$(echo "$commit_msg" | head -1)
 
@@ -22,8 +20,9 @@ commit_msg_head=$(echo "$commit_msg" | head -1)
 checkBlank() {
   if [ ${commit_line_num} -eq 0 ]
   then
-    echo -e "${COLOR_RED}\nCommit log error: Commit message can not be empty." >&2
-    echo -e "${COLOR_NONE} - Refer commit guide: ${refer_commit_guide}\n" >&2
+    echo "\nCommit log error: Commit message can not be empty." >&2
+    echo "    - Refer commit guide: ${refer_commit_guide}" >&2
+    echo "    - Message generation plug-in $plugin\n" >&2
     exit 1
   fi
 }
@@ -37,8 +36,9 @@ checkHeaderFormat() {
   local count=$(echo "${commit_msg_head}" | grep -Ec "$header_regex")
   if [ ${count} -eq 0 ]
   then
-    echo -e "${COLOR_RED}\nCommit log error: First commit message line (commit header) does not follow format: type(scope): subject" >&2
-    echo -e "${COLOR_NONE} - Refer commit guide: ${refer_commit_guide}\n" >&2
+    echo "\nCommit log error: First commit message line (commit header) does not follow format: type(scope): subject" >&2
+    echo "    - Refer commit guide: ${refer_commit_guide}" >&2
+    echo "    - Message generation plug-in $plugin\n" >&2
     exit 1
   fi
 
@@ -56,8 +56,9 @@ checkHeaderType() {
   local count=$(echo "${commit_msg_head}" | grep -Ec "$header_regex")
   if [ ${count} -eq 0 ]
   then
-    echo -e "${COLOR_RED}\nCommit log error: Commit type illegal, expected: ${COMMIT_TYPE_ARRAR[@]}" >&2
-    echo -e "${COLOR_NONE} - Refer commit guide: ${refer_commit_guide}\n" >&2
+    echo "\nCommit log error: Commit type illegal, expected: ${COMMIT_TYPE_ARRAR[@]}" >&2
+    echo "    - Refer commit guide: ${refer_commit_guide}" >&2
+    echo "    - Message generation plug-in $plugin\n" >&2
     exit 1
   fi
 }
@@ -72,8 +73,9 @@ checkSecondLine() {
     local second_line_msg=$(echo "$commit_msg" | head -n 2 | tail -n 1 | tr -d " ")
     if [ "${second_line_msg}" != "" ]
     then
-      echo -e "${COLOR_RED}\nCommit log error: Commit second line must be blank" >&2
-      echo -e "${COLOR_NONE} - Refer commit guide: ${refer_commit_guide}\n" >&2
+      echo "\nCommit log error: Commit second line must be blank" >&2
+      echo "    - Refer commit guide: ${refer_commit_guide}" >&2
+      echo "    - Message generation plug-in $plugin\n" >&2
       exit 1
     fi
   fi
@@ -90,8 +92,9 @@ checkBody() {
     local third_line_msg=$(echo "$commit_msg" | head -n 3 | tail -n 1 | tr -d " ")
     if [ "${third_line_msg}" = "" ]
     then
-      echo -e "${COLOR_RED}\nCommit log error: Commit third line(commit body) could not be blank" >&2
-      echo -e "${COLOR_NONE} - Refer commit guide: ${refer_commit_guide}\n" >&2
+      echo "\nCommit log error: Commit third line(commit body) could not be blank" >&2
+      echo "    - Refer commit guide: ${refer_commit_guide}" >&2
+      echo "    - Message generation plug-in $plugin\n" >&2
       exit 1
     fi
   fi
